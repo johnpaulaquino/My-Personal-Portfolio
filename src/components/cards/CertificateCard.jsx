b7d1cbe057c660ebe01861479e48e115586c5d67
Paul <forgames.app2022@gmail.com>
2025-06-15 12:44:20 +0800

    .

--- src/components/cards/CertificateCard.jsx
+++ src/components/cards/CertificateCard.jsx
@@ -6,7 +6,7 @@ const CertificateCards = ({ data }) => {
         <div className="w-full h-auto flex">
           <div className="w-full h-auto relative  rounded-[10px] p-[30px] border-2 border-[#2E2E2E] bg-[#2e2e2e49]">
             <div className="absolute right-0 top-0 w-[100px] m-[20px]">
-              <img src="/src//assets/certificate_logo.png" alt="Certificate Logo" />
+              <img src="/src/sssassets/certificate_logo.png" alt="Certificate Logo" />
             </div>
 
             <div className="flex w-full h-auto mt-[90px] flex-col items-center justify-center">
